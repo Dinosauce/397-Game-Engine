3c4652f3f469655a0bd1d34c6708cd9b68efd573
Jayden Grant <dinosauce@live.com.au>
2017-04-26 13:12:12 +0800

    Init Glew

--- ICT397_Assignment/ICT397_Assignment/Graphics.cpp
+++ ICT397_Assignment/ICT397_Assignment/Graphics.cpp
@@ -32,6 +32,8 @@ void OpenGL::CreateGameWindow(int width, int height, char* window_name, int* arg
 void OpenGL::Initialize(){
 	// set background (sky colour)
 	glClearColor(97.0 / 255.0, 140.0 / 255.0, 185.0 / 255.0, 1.0);
+	glewInit();
+	
 
 	ObjectGrids.LoadGameObject("3Dmodels/grid.obj");
 
@@ -39,7 +41,11 @@ void OpenGL::Initialize(){
 
 void OpenGL::Display(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
 
+	gluLookAt(-150, 100, 50,
+		0, 40, -20,
+		0, 1, 0);
 
 	IncrementFrameCount();
 
@@ -64,8 +70,9 @@ void OpenGL::Reshape(int width, int height){
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glViewport(0, 0, screen_width, screen_height);
-	gluPerspective(45.0, ratio, 0.1f, 1000.0f);
+	gluPerspective(90.0, ratio, 0.1f, 1000.0f);
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void OpenGL::IncrementFrameCount(){
