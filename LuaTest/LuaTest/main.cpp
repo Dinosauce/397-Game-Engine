0f6584306f9f95d64fef6bda49f2e4994328e255
Veggipuppet <Veggipuppet@users.noreply.github.com>
2017-05-29 16:28:39 +0800

    Created a Lua class to handle the IO
    makes it very simple to read in from the lua files

--- LuaTest/LuaTest/main.cpp
+++ LuaTest/LuaTest/main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include "lua_script\C\LuaEngine.h"
 #include "Player.h"
-
+#include "LuaScript.h"
+/*
 int main(int argc, char* argv[]){
 	LuaEngine lua_engine;
 	Player player(lua_engine.GetPlayer());
@@ -10,4 +11,39 @@ int main(int argc, char* argv[]){
 
 	getchar();
 	return 0;
+}*/
+
+int main() {
+	LuaScript script("Player.lua");
+	float posX = script.get<float>("player.position.x");
+	float posY = script.get<float>("player.position.y");
+	float posZ = script.get<float>("player.position.z");
+	std::string filename = script.get<std::string>("player.filename");
+	int hp = script.get<int>("player.HP");
+
+	std::cout << "Position X = " << posX << ", Y = " << posY <<", Z = " << posZ << std::endl;
+	std::cout << "Filename:" << filename << std::endl;
+	std::cout << "HP:" << hp << std::endl;
+
+	// getting arrays
+	std::vector<int> v = script.getIntVector("array");
+	std::cout << "Contents of array:";
+	for (std::vector<int>::iterator it = v.begin();
+		it != v.end();
+		it++) {
+		std::cout << *it << ",";
+	}
+	std::cout << std::endl;
+
+	// getting table keys:
+	std::vector<std::string> keys = script.getTableKeys("player");
+	std::cout << "Keys of [player] table:";
+	for (std::vector<std::string>::iterator it = keys.begin();
+		it != keys.end();
+		it++) {
+		std::cout << *it << ",";
+	}
+	std::cout << std::endl;
+
+	getchar();
 }
